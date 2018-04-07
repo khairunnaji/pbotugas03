4b2ab16f9ad848647929f015be6d6cf040fddf71
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-04-07 14:21:52 +0800

    membuat method cetakinfo

--- src/pbotugas03/Pakaian.java
+++ src/pbotugas03/Pakaian.java
@@ -2,5 +2,15 @@
 package pbotugas03;
 
 public class Pakaian {
-
+  String kain;
+    String warna;
+    String ukuran;
+    Double harga;
+    
+    void cetakInfo(){
+        System.out.print("kain \t: "+kain+"\n"+
+                "warna \t: "+warna+"\n"+
+                "ukuran \t: "+ukuran+"\n"+
+                "harga \t: "+harga+"\n");
+}
 }
