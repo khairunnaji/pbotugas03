01b5c58d038563a3ff90e2ec314ba80f90b74321
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-04-07 14:29:59 +0800

    set attribut private

--- src/pbotugas03/Pakaian.java
+++ src/pbotugas03/Pakaian.java
@@ -2,10 +2,10 @@
 package pbotugas03;
 
 public class Pakaian {
-  String kain;
-    String warna;
-    String ukuran;
-    double harga;
+ private String kain;
+ private   String warna;
+ private   String ukuran;
+ private   double harga;
     
     void cetakInfo(){
         System.out.print("kain \t: "+kain+"\n"+
