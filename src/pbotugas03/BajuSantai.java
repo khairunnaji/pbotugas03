955c434a9fbcef104626bb64d1eb645afc934569
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-04-07 15:12:39 +0800

    Geeter and setter

--- src/pbotugas03/BajuSantai.java
+++ src/pbotugas03/BajuSantai.java
@@ -4,4 +4,13 @@ public class BajuSantai extends Pakaian{
      void throttle(){
         System.out.println("TinggiSekaliiiii.....");
     }
+
+    public String getKerah() {
+        return Kerah;
+    }
+
+    public void setKerah(String Kerah) {
+        this.Kerah = Kerah;
+    }
+
 }
