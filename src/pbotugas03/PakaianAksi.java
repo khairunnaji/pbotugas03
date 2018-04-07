baf95d195276d7eca5645a791139a2000716d218
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-04-07 14:32:57 +0800

    menggunakan method set

--- src/pbotugas03/PakaianAksi.java
+++ src/pbotugas03/PakaianAksi.java
@@ -3,10 +3,10 @@ package pbotugas03;
 public class PakaianAksi {
     public static void main(String[] args) {
      Pakaian p = new Pakaian();
-     p.kain="tenun";
-     p.warna="jingga";
-     p.ukuran="L";
-     p.harga=250000;
+p.setKain("tenun");
+p.setWarna("warna");
+p.setUkuran("L");
+p.setHarga(250000);
              
              p.cetakInfo();
     }
