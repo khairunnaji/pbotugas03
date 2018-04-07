339d3e6d303618d7cf8c063f66badf32fbfe9f7f
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-04-07 14:53:12 +0800

    Artibut dan metthod

--- src/pbotugas03/BajuKantor.java
+++ src/pbotugas03/BajuKantor.java
@@ -1,5 +1,10 @@
 
 package pbotugas03;
 public class BajuKantor extends Pakaian {
-    
+     private String motiv;
+     
+     void throttle(){
+        System.out.println("Sasirangannnn");
+         
+     }
 }
