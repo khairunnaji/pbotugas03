a42159e88025d0f143639844b5ef13c546e707bb
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2018-04-07 15:11:21 +0800

    attribut dan method

--- src/pbotugas03/BajuSantai.java
+++ src/pbotugas03/BajuSantai.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pbotugas03;
-
-/**
- *
- * @author puden
- */
-public class BajuSantai {
-    
+public class BajuSantai extends Pakaian{
+    private String Kerah; 
+     void throttle(){
+        System.out.println("TinggiSekaliiiii.....");
+    }
 }
